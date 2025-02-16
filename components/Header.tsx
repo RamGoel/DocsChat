b130eab5b4645f56d0e11e1d7381586328f5399b
Ram Goel <rgoel766@gmail.com>
2025-02-16 14:48:05 +0530

    fix: meta info

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="w-[60%] bg-neutral-800 rounded-bl-xl rounded-br-xl border-2 border-t-0 border-neutral-900 flex items-center justify-between mx-auto p-4">
       <h1 className="text-2xl">CrustChat</h1>
 
-      <p>Ask me anything about Crusdata APIs</p>
+      <p>Ask me anything about Crustdata APIs</p>
     </div>
   );
 };
